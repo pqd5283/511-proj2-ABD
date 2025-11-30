128380e20fe20c9de82db5d338d2fd123bd8f4d6
pqd5283 <pqd5283@psu.edu>
2025-11-29 19:03:32 -0500

    wrote up the server code, again used a lot of the code from the last project, also added params to the server c functions so that the server can use them, not sure what my next move is but its gonna be either write up the server c code or client c code

--- server/server_funcs.c
+++ server/server_funcs.c
@@ -2,11 +2,22 @@
 #include "server_rpc.h"
 
 
-int server_init(){}
+int server_init();
 
-int server_receive_read(){}
+int server_receive_read(int *out_key,
+                    char *out_value,
+                    size_t out_value_size){}
 
-int server_receive_write(){}
+int server_read_writeback(int *out_key,
+                    char *out_value,
+                    size_t out_value_size){}
+
+int server_receive_write(int *out_key,
+                    char *out_value){}
+
+int server_write_writeback(int key,
+                               char *value,
+                               char *client_id){}
 
 // int server_lock();
 // int server_unlock();
---
+++ server/server_rpc.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "../proto/abdalgorithm.grpc.pb.h"
+#include "server_rpc.h"
+#include "server_funcs.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using abd::ClientRead;
+using abd::ClientReadWriteback;
+using abd::ClientWrite;
+using abd::ClientWriteback;
+using abd::read_request;
+using abd::read_reply;
+using abd::read_writeback;
+using abd::read_writeback_reply;
+using abd::write_request;
+using abd::write_reply;
+using abd::writeback_request;
+using abd::writeback_reply;
+
+// a lot of this code is yet again taken from and modified from the last project 
+//server read 
+class ClientReadService final : public ClientRead::Service {
+public:
+    Status SendRead(ServerContext* context,
+                    const read_request* request,
+                    read_reply* reply) override {
+
+        int key = 0;
+        char value_buf[1024]; // 1024 max for now 
+
+        if (server_receive_read(&key, value_buf, sizeof(value_buf)) != 0) {
+            return Status(grpc::StatusCode::INTERNAL, "abd_handle_read failed");
+        }
+
+        reply->set_key(key);
+        reply->set_value(value_buf);
+        return Status::OK;
+    }
+};
+
+
+
+//server read writeback
+class ClientReadWritebackService final : public ClientReadWriteback::Service {
+public:
+    Status SendReadWriteback(ServerContext* context,
+                             const read_writeback* request,
+                             read_writeback_reply* reply) override {
+
+        int key = request->key();
+        const std::string& value = request->value();
+
+
+        int check = server_read_writeback(key, value.c_str());
+        reply->set_success(check == 0);
+
+        return Status::OK;
+    }
+};
+
+
+//server write
+class ClientWriteService final : public ClientWrite::Service {
+public:
+    Status SendWrite(ServerContext* context,
+                     const write_request* request,
+                     write_reply* reply) override {
+
+        int key = 0;
+        char value_buf[1024];
+
+        if (server_receive_write(&key, value_buf, sizeof(value_buf)) != 0) {
+            return Status(grpc::StatusCode::INTERNAL, "abd_handle_write_query failed");
+        }
+
+        reply->set_key(key);
+        reply->set_value(value_buf);
+        return Status::OK;
+    }
+};
+
+
+//server write writeback
+class ClientWritebackService final : public ClientWriteback::Service {
+public:
+    Status SendWriteback(ServerContext* context,
+                         const writeback_request* request,
+                         writeback_reply* reply) override {
+
+        int key = request->key();
+        const std::string& value = request->value();
+        const std::string& client_id = request->client_id();
+
+        int check = server_write_writeback(key, value.c_str(), client_id.c_str());
+        reply->set_success(check == 0);
+
+        return Status::OK;
+    }
+};
+
+// setup and run the server
+void RunABDServer(const std::string& address) {
+    ClientReadService read_service;
+    ClientReadWritebackService readwb_service;
+    ClientWriteService write_service;
+    ClientWritebackService writewb_service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&read_service);
+    builder.RegisterService(&readwb_service);
+    builder.RegisterService(&write_service);
+    builder.RegisterService(&writewb_service);
+
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    server->Wait();
+}
+
+// main function to start the server
+int main(int argc, char** argv) {
+    std::string port = "50051";
+    if (argc > 1) {
+        port = argv[1];
+    }
+
+    std::string server_address = "0.0.0.0:" + port;
+    RunABDServer(server_address);
+    return 0;
+}
