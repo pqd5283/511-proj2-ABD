11ff6b26481b78eabc78b4babd72f4a6ca6aa75a
pqd5283 <pqd5283@psu.edu>
2025-12-02 17:35:48 -0500

    another small config error, see if it fixes it

--- server/server.h
+++ server/server.h
@@ -14,15 +14,15 @@ int server_receive_read(int *out_key,
                     size_t out_value_size);
 
 int server_read_writeback(int key,
-                    char *out_value);
+                    const char *out_value);
 
 int server_receive_write(int *out_key,
                     char *out_value,
                     size_t out_value_size);
 
 int server_write_writeback(int key,
-                               char *value,
-                               char *client_id);
+                               const char *value,
+                               const char *client_id);
 
 // int server_lock();
 // int server_unlock();
